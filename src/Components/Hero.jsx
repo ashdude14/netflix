a9055c7d60e4c4ebfad976c0e4a784c1a5de1915
Ashish Kumar <ashish.kumar.singh.jee@gmail.com>
2024-01-05 11:35:39 +0530

    hero div center hor+ver

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-    <div className=' text-white flex items-center justify-center h-screen w-screen'>
-      <h1 className=' font-extralight text-[28px] sm:text-[32px] md:text-[45px] '>Unlimited movies, 
-      TV shows and more</h1>
-      <p className='text-white text-center justify-center absolute mt-[110px] text-[20px] sm:text-[29px] md:text-[30px]'>
-      Watch anywhere. Cancel anytime.
+    <div className='text-white flex flex-col items-center justify-center h-screen'>
+      <h1 className='font-extralight text-[28px] sm:text-[32px] md:text-[45px] text-center'>
+        Unlimited movies, TV shows and more
+      </h1>
+      <p className='text-[20px] sm:text-[29px] md:text-[30px] text-center mt-4'>
+        Watch anywhere. Cancel anytime.
       </p>
-      <h3 className='absolute  text-center justify-center mt-[250px] text-[20px] sm:text-[29px] md:text-[30px]'>
-      Ready to watch? Enter your email to create or restart your membership.
+      <h3 className='text-[20px] sm:text-[29px] md:text-[30px] text-center mt-8'>
+        Ready to watch? Enter your email to create or restart your membership.
       </h3>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
