06b009d90470f2b9870767de3b349339c9411a1f
Ashish Kumar <ashish.kumar.singh.jee@gmail.com>
2024-01-09 20:40:37 +0530

    home-page/ui

--- src/Components/FAQs.jsx
+++ src/Components/FAQs.jsx
@@ -1,133 +1,66 @@
 import React, { useEffect, useRef, useState } from 'react'
-
+import {data} from "./data.jsx"
 const FAQs = () => {
-     const [click,setClick]=useState(false);
+     const [click,setClick]=useState(null);
     
-   function handleClick()
-    {
-        console.log(`clicked ${click} `);
-        setClick(!click);
-    }
-   
-       
-    
-  return (
-       
+   const handleClick = (i) =>
+    { if(click===i)
+       return setClick(null);
+      setClick(i);
+    }    
+  return (   
     <div>
         <section className=" w-[100wv]  bg-black border-t-[5px] border-t-slate-700
      text-white sm:py-[50px] py-[70px] ">
          <div className=' max-w-[1170px] mx-auto px-[15px]' > 
       <h1 className='text-white  text-center  sm:text-[50px] 
       text-[40px] font-bold sm:leading-[65px] leading-[50px] py-4'>Frequently Asked Questions</h1>
-      <div className='bg-[#2d2d2d] mb-[20px]'>
-        <div className=' faqQs relative text-white text-[30px] p-[20px] border-[2px] border-black 
-        hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer 'onClick={handleClick} > <h3>What is Netflix?</h3>
-          <span className=' absolute right-[20px] top-[10px] text-[40px] align-middle'>
-          {click ? 'x' : '+'}
-            </span> 
-        </div>
-      <div className={` ${click ? 'p-[20px] duration-200' : ' text-[20px] scale-y-0 h-0 origin-top '} `}>
+   { 
+   data.map((item,index)=>(
+    <div className='bg-[#2d2d2d] mb-[20px]'>
+   <div className=' relative text-white text-[30px] p-[20px] border-[2px] border-black 
+   hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer 'onClick={()=>handleClick(index)}  > <h3> {item.quest}</h3>
+    <span className=' absolute right-[20px] top-[10px] text-[40px] align-middle'>
+          {click === index? 'x' : '+'}
+            </span>
+            </div>
+           <div className={` ${click === index ? 'p-[20px] duration-350' : ' text-[40px] scale-y-0 h-0 origin-top '} `}>
           <h3>
-            <p>
-            Netflix is a streaming service that offers a wide variety 
-            of award-winning TV shows, movies, anime, documentaries and more – 
-            on thousands of internet-connected devices.
-         </p> <p>
-           You can watch as much as you want, whenever 
-           you want, without a single ad – all for one low monthly price. 
-           There's always something new to discover, and new TV shows
-            and movies are added every week!
-            </p>
+          
+            {item.answer}
+            
           </h3>
       </div>
-   {/* q2 */}
-      <div className=' faqQs relative text-white text-[30px] p-[20px] border-[2px] border-black 
-        hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer 'onClick={()=>(setClick(!click))} > <h3>How much does Netflix cost?</h3>
-          <span className=' absolute right-[20px] top-[10px] text-[40px] align-middle'>
-          {click ? 'x' : '+'}
-            </span> 
-        </div>
-      <div className={` ${click ? 'p-[20px] duration-200' : ' text-[20px] scale-y-0 h-0 origin-top '} `}>
-          <h3>
-            <p>
-            Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from 
-            <br/> ₹149 to ₹649 a month. No extra costs, no contracts.
-         </p>
-          </h3>
       </div>
-   
-   {/* q 3*/ }
-   <div className=' faqQs relative text-white text-[30px] p-[20px] border-[2px] border-black 
-        hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer 'onClick={()=>(setClick(!click))} > <h3>Where can I watch?</h3>
-          <span className=' absolute right-[20px] top-[10px] text-[40px] align-middle'>
-          {click ? 'x' : '+'}
-            </span> 
-        </div>
-      <div className={` ${click ? 'p-[20px] duration-200' : ' text-[20px] scale-y-0 h-0 origin-top '} `}>
-          <h3>
-            <p>
-            Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
+   ))}
 
-<br/> You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.
-            </p>
-          </h3>
-      </div>
-      
-   {/* q 4*/ }
-   <div className=' faqQs relative text-white text-[30px] p-[20px] border-[2px] border-black 
-        hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer 'onClick={()=>(setClick(!click))} > <h3>How do I cancel?</h3>
-          <span className=' absolute right-[20px] top-[10px] text-[40px] align-middle'>
-          {click ? 'x' : '+'}
-            </span> 
-        </div>
-      <div className={` ${click ? 'p-[20px] duration-200' : ' text-[20px] scale-y-0 h-0 origin-top '} `}>
-          <h3>
-            <p>
-            Netflix is flexible. There are no annoying contracts and no commitments.
-             You can easily cancel your account online in two clicks. 
-             There are no cancellation fees – start or stop your account anytime.
-            </p>
-          </h3>
-      </div>
-
-   {/* q 5*/ }
-   <div className=' faqQs relative text-white text-[30px] p-[20px] border-[2px] border-black 
-        hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer 'onClick={()=>(setClick(!click))} > <h3>What can I watch on Netflix?</h3>
-          <span className=' absolute right-[20px] top-[10px] text-[40px] align-middle'>
-          {click ? 'x' : '+'}
-            </span> 
-        </div>
-      <div className={` ${click ? 'p-[20px] duration-200' : ' text-[20px] scale-y-0 h-0 origin-top '} `}>
-          <h3>
-            <p>
-          Netflix has an extensive library of feature films, documentaries, TV shows, anime,
-           award-winning Netflix originals, and more. Watch as much as you want, anytime you want.
-            </p>
-          </h3>
-      </div>
-     
-   {/* q 6*/ }
-   <div className=' faqQs relative text-white text-[30px] p-[20px] border-[2px] border-black 
-        hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer 'onClick={()=>(setClick(!click))} > <h3> Is Netflix good for kids?</h3>
-          <span className=' absolute right-[20px] top-[10px] text-[40px] align-middle'>
-          {click ? 'x' : '+'}
-            </span> 
-        </div>
-      <div className={` ${click ? 'p-[20px] duration-200' : ' text-[20px] scale-y-0 h-0 origin-top '} `}>
-          <h3>
-            <p>
-            The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.
-
-Kids profiles come with PIN-protected parental
-controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.
-            </p>
-          </h3>
-      </div>
-    </div>
-    </div>
+<h3 className='text-[20px] sm:text-[29px] md:text-[30px] text-center mt-[10px]'>
+        Ready to watch? Enter your email to create or restart your membership.
+      </h3>
+         <div className="flex max-w-lg gap-2 mx-auto mt-8">
+      <input
+        type="text"
+        id="email"
+        name="email"
+        className="form-input w-[200px] sm:w-[400px] border
+         border-white bg-[transparent] text-white sm:text-[20px]
+        text-[15px]  p-2 rounded-l-lg focus:outline-none focus:border-white border-spacing-2"
+        placeholder="Email address"
+      />
+      <button
+        type="button"
+        className="bg-red-700 h-[50px] w-[200px]
+         sm:w-[350px] text-white text-sm font-bold 
+         sm:text-[20px]
+         rounded-r-md hover:bg-red-800 focus:outline-none 
+         focus:shadow-outline-red active:bg-red-900"
+      >
+        Get Started ≥
+      </button>
+     </div>
+      </div>  
     </section>
-    </div>
-  )
-}
 
+    </div>
+  )}
 export default FAQs
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
-
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
   return (
     <div>
     <section className=" w-[100wv]  bg-black border-t-[5px] border-t-slate-700
  text-white sm:py-[50px] py-[70px] ">
-     <div className=' max-w-[1170px] mx-auto px-[15px] ' > 
-  <h1 className='text-white  text-center sm:text-[50px] text-[40px] font-bold sm:leading-[65px] leading-[50px]'>This is FOOOOTEEEREE!!!!!!!</h1>
+     <div className='flex max-w-[1170px] mx-auto px-[15px] item-center justify-between gap-10' > 
+  <h1 className='text-white '>Author - Aashish Kumar Singh</h1>
+  <h1 className='text-white'> Copyright © {new Date().getFullYear()}. All rights are reserved.</h1>
+  <div className=" items-center ">
+       <h4 className='text-white '>let's connect!</h4>
+       <h4> LinkedIn</h4>
+                  <a
+                    aria-label="linkedin"
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/aashish-kumar-singh-499241164//"
+                  >
+                    <FaLinkedin width={32} height={32} />
+                  </a>
+                 
+                  <a
+                    aria-label="github"
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://github.com/ashdude14"
+                  >
+                     <h4> GitHub</h4>
+                    <FaGithub width={32} height={32} />
+                  </a>
+        
+            </div>
 </div>
 </section>
 </div>
